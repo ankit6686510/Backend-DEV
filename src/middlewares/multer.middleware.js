e5af5650a4355da44b689d91aa2908f600994944
Ankit Jha <ankit6686510@gmail.com>
2024-08-18 21:58:53 +0530

    multer and cloudinary

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,17 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    //cb is callback
+    cb(null, "./public/temp");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.originalname);
+  },
+});
+
+export const upload = multer({ 
+    // storage: storage or
+    storage,
+});
