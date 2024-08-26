920dbb2034101d9ee15609098cd0ed0f8de47838
Ankit Jha <ankit6686510@gmail.com>
2024-08-26 23:35:34 +0530

    add register controller and bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,24 @@
-import { Router } from "express";
-import { registerUser } from "../controllers/user.conntroller.js";
+import { Router } from 'express';
+import { registerUser } from "../controllers/user.controller.js";
+
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = Router()
 
-router.route("/register").post(registerUser)
+router.route("/register").post(
+    upload.fields([
+        {
+            name:"avatar",
+            maxCount:1
+        },
+        {
+            name:"coverImage",
+            maxCount:1
+        }
 
-export {router}
-// export default (router)
+    ]),
+    registerUser
+)
+
+// export {router}
+export default (router)
